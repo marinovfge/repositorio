fb0b4c8bc24c27513d33bb6d5625e01eef22f4fc
LeninGF <enteatenea@gmail.com>
2023-03-15 08:08:37 -0500

    lectura de secret key dese dotenv

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,12 +1,15 @@
 import os
 from flask import Flask
 from sqlalchemy import create_engine
-
+from dotenv import load_dotenv
 app = Flask(__name__)
-app.secret_key = '20485ba791e947005ef076f6eb4ad9d3b1dcb87c0fd3bd95bd2b384059b3c874'
-app.config['SQLALCHEMY_DATABASE_URI'] = ("mysql+pymysql://"+ os.environ['DB_USER'] + ":"
-                                         + os.environ['DB_PASSWORD']+ "@"
-                                         + os.environ['DB_HOST']
+app.secret_key = os.getenv('APP_SECRET_KEY')
+
+# alternatively using os.environ[var] which reads os, but os.getenv reads file dot env
+app.config['SQLALCHEMY_DATABASE_URI'] = ("mysql+pymysql://"+ os.getenv('DB_USER') + ":"
+                                         + os.getenv('DB_PASSWORD')+ "@"
+                                         + os.getenv('DB_HOST')
                                          + ":3306")
+
 mariadb = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
 print(f"conectado a bbdd: {mariadb.connect()}")
