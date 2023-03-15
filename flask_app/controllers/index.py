ee6f0ea7466dfcbb89d9e48643cd7db67612ecc5
LeninGF <enteatenea@gmail.com>
2023-03-15 10:44:25 -0500

    adding method get

--- flask_app/controllers/index.py
+++ flask_app/controllers/index.py
@@ -2,6 +2,6 @@ from flask_app import app
 from flask import render_template, redirect, flash, session, request
 from urllib import request
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     return render_template("index.html")
