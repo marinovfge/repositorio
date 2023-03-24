d58042f02d83fea7641de0761b52aeffe94c3217
LeninGF <enteatenea@gmail.com>
2023-03-24 08:27:29 -0500

    metodo para manejar la pagina acerca datos

--- flask_app/controllers/dashboards.py
+++ flask_app/controllers/dashboards.py
@@ -2,6 +2,7 @@ from flask_app import app
 from flask_app.models.dashboard import Dashboard
 from flask import render_template, redirect, flash, session, request
 from urllib import request
+from datetime import datetime
 
 
 @app.route('/dashboard-general')
@@ -20,4 +21,9 @@ def dashboard_general_fge():
     fecha_corte = Dashboard.get_fecha_corte()
     return render_template("dashboard_general_fge.html", fecha_corte = fecha_corte)
 
+@app.route('/acerca-de-los-datos')
+def acerca_datos():
+    fecha_corte = Dashboard.get_fecha_corte()
+    fecha_ini = datetime(2015,1,1).strftime("%d/%m/%Y")
+    return render_template("acerca_datos.html", fecha_corte=fecha_corte, fecha_ini=fecha_ini)
 
