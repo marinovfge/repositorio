96a1e375395f8c4788c7f57d575ad93d943a42e1
LeninGF <enteatenea@gmail.com>
2024-04-29 20:10:51 -0500

    rutina mostrar nota robos

--- flask_app/controllers/dashboards.py
+++ flask_app/controllers/dashboards.py
@@ -54,6 +54,13 @@ def acerca_datos():
     total_registros = Dashboard.get_total_registros_open_data()
     return render_template("acerca_datos.html", fecha_corte=fecha_corte, fecha_ini=fecha_ini, total_registros=total_registros)
 
+@app.route('/acerca-de-los-datos-robos')
+def acerca_datos_robo():
+    fecha_corte = Dashboard.get_fecha_corte()
+    fecha_ini = datetime(2015,1,1).strftime("%d/%m/%Y")
+    total_registros = Dashboard.get_total_registros_open_data()
+    return render_template("acerca_datos_robos.html", fecha_corte=fecha_corte, fecha_ini=fecha_ini, total_registros=total_registros)
+
 @app.route('/reporte-crear-femicidio', methods=['GET'])
 def dashboard_crear_reporte_femicidio():
     """_summary_
